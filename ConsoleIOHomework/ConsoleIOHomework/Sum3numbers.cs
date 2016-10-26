433eeae0183c62dc4ea17a800679df90f15ff55e
Milena Ivanova <milena_ii@abv.bg>
2016-10-26 10:58:51 +0300

    Console Input Output Homework

---
+++ ConsoleIOHomework/ConsoleIOHomework/Sum3numbers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Sum3numbers
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+        int d = a + b + c;
+        Console.WriteLine(d);
+    }
+}
+
