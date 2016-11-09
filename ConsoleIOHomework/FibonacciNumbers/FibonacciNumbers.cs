c25731cf2431c43f73d84062d4f8dfdc1bb2f2c3
Milena Ivanova <milena_ii@abv.bg>
2016-11-09 18:02:52 +0200

    Resolved task "Fibonacci" and "Interval".

---
+++ ConsoleIOHomework/FibonacciNumbers/FibonacciNumbers.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class FibonacciNumbers
+{
+    static void Main()
+    {
+        //int n = int.Parse(Console.ReadLine());
+        //int a = 0;
+        //int b = 1;
+        //    Console.Write("0,1,");
+        //for (int i = 1; i < ((n/2)-2); i++)
+        //{ 
+        //    a = a + b;
+        //    b = a + b;
+        //    Console.Write("{0},{1},", a, b);
+        //}
+
+
+        int n = int.Parse(Console.ReadLine());
+        for (uint i = 0, a = 0, b = 1; i < n; i++, a = a + b, b = a - b)
+        {
+            Console.Write(a + ", ");
+            Console.WriteLine(b);
+
+        //i = 0, a = 0, b = 1;
+        //i = 1, a = 1, b = 0; 
+        //i = 2, a = 1, b = 1;
+        //i = 3, a = 2, b = 1;
+        //i = 4, a = 3, b = 2;
+
+        //или b = на настоящето а - миналото b
+        //        b e последното число
+        //        а е сборът, т.е. новото число
+        //    a + b = най-новото
+
+
+        //Трети начин
+        //                    Console.Write("Enter a number N for range: ");
+        //    BigInteger n = BigInteger.Parse(Console.ReadLine());
+        //    BigInteger firstNum = 0;
+        //    BigInteger secondNum = 1;
+        //    BigInteger thirdNum = 1;
+        //    for (int i = 0; i < n; i++)
+        //    {
+        //        thirdNum = secondNum;
+        //        secondNum = firstNum + secondNum;
+        //        firstNum = thirdNum;
+        //        Console.Write(firstNum + " ");
+        //    }
+        //    Console.ReadLine();
+
+
+    }
+
+
+
+
---
+++ ConsoleIOHomework/Interval/Interval.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Interval
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter two integer positive number: ");
+        uint n = uint.Parse(Console.ReadLine());
+        uint m = uint.Parse(Console.ReadLine());
+        if (!(0 <= n) ^ !(n <= m) ^ !(m <= 2000))
+        {
+            Console.WriteLine("Not valid numbers!");
+        }
+        else
+        {
+            uint a = (m - n) / 5;
+            Console.WriteLine(a);
+        }
+    }
+}
+
