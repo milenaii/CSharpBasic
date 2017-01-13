318e1676435711ebfbc87ef549cd55a43b1e5c30
Milena Ivanova <milena_ii@abv.bg>
2017-01-13 16:23:15 +0200

    Some tasks..

--- ArraysHomework/15.PrimeNumbers/PrimeNumbers.cs
+++ ArraysHomework/15.PrimeNumbers/PrimeNumbers.cs
@@ -2,26 +2,49 @@
 //Write a program that finds all prime numbers in the range [1 ... N].
 //Use the Sieve of Eratosthenes algorithm. The program should print the
 //biggest prime number which is <= N.
+
+// info - algorithm https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("Enter n: ");
+        Console.WriteLine("Enter an integer n: ");
         int n = int.Parse(Console.ReadLine());
 
-        int divise = 2;
-        for (int i = 2; i <= n; i++)
+        bool[] A = new bool[n+1];
+
+        for (int i = 0; i < A.Length; i++)
         {
-            if (n % divise == 0)
+            A[i] = true;
+        }
+
+        for (int i = 2; i < Math.Sqrt(n); i++)
+        {
+            if (A[i] == true)
             {
-                Console.WriteLine("{0} is not prime, b/e it is devided on {1}",n, divise);
-                divise++;
+                for (int j = i * i; j < n; j = j + i)
+                {
+                    A[j] = false;
+                }
             }
-            else
+        }
+        //This code prints all prime numbers from 1 up to n
+
+        //for (int i = 0; i < n; i++)
+        //{
+        //    if (A[i] == true)
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+        //}
+
+        for (int i = n ; i >= 2; i--)   // prints the biggest
+        {                                  // of prime numbers
+            if (A[i] == true)
             {
-                Console.WriteLine("prime");
+                Console.WriteLine(i);
+                break;
             }
         }
     }
 }
-
